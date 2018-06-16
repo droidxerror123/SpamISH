27b4c10d3393a0d3b092ff3fd84f8ee15c9bd4d2
Droid <39285561+droidxerror123@users.noreply.github.com>
2018-06-16 15:35:40 +0000

    Create ISH.sh

---
+++ ISH.sh
@@ -0,0 +1,67 @@
+#!/data/data/com.termux/files/usr/bin/bash
+# Auto gelud
+# coded By ./Droid
+# Bersihkan Layar
+clear
+
+blue='\e[0;34'
+cyan='\e[0;36m'
+green='\e[0;34m'
+okegreen='\033[92m'
+lightgreen='\e[1;32m'
+white='\e[1;37m'
+red='\e[1;31m'
+yellow='\e[1;33m'
+sleep 1
+toilet -f standard -F gay "T.HUNTER"
+echo ""
+
+###################################################
+# CTRL C
+###################################################
+trap ctXrl_c INT
+ctrl_c() {
+clear
+echo -e $gold"[#]> (Ctrl + C ) Detected, Trying To Exit ... "
+sleep 1
+echo ""
+echo -e $blue"[#]> semoga bermanfaat "
+sleep 1
+echo ""
+read enter
+exit
+}
+
+echo -e $red"================================================"     
+echo -e $yellow"=Author :./Droid_X_ISH&Tn.Chaplin&Kuro Cyber  =" 
+echo -e $red"=Team   :Indonesian Security Hunter           ="
+echo -e $yellow"=Kontak :089606402413                         ="
+echo -e $red"=Date   :05-06-2018                            ="
+echo -e $yellow"================================================"
+echo ""
+echo -e $red"××××××××××××××××××××××××××××××××××××"
+echo "01)Telkomsel"
+echo "02)jd.id"
+echo "03)toko pedia"
+echo "04)Tri"
+echo "05)Spam telepon"
+echo "06)matahari mall"
+echo "00)keluar"
+echo -e $white""
+read -p "[ISH==> " act;
+
+if
+[ $act = 01 ] || [ $act = 01 ]
+then
+clear
+toilet "T-Sel"
+php 1.php
+fi
+
+if
+[ $act= 02 ] || [ $act = 02 ]
+then
+clear
+toilet -f mono12 -F gay "Jd.Id"
+php 2.php
+fi
